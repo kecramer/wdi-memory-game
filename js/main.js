117cbc830025618522dcad0b09919ceecb413593
Kevin Cramer <kcramer@gmail.com>
2018-03-30 18:39:07 -0700

    Unit 8 changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-var cardOne = "queen", cardTwo = "queen", cardThree = "king", cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0], cardTwo = cards[2];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you found a match!");
+	} else {
+		alert("sorry, try again!")
+	}
+}
