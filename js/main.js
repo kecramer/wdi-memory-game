6aeaaff0eb154b10c84c7999602756ed319d2945
Kevin Cramer <kcramer@gmail.com>
2018-04-12 06:22:16 -0700

    Reset board when match not made

--- js/main.js
+++ js/main.js
@@ -2,31 +2,42 @@ var cards = [
 {
 	rank: "queen",
 	suite: "hearts",
-	cardImage: "images/queen-of-hearts.png"
+	cardImage: "images/queen-of-hearts.png",
+	cardId: 0,
+	matched: false
 },
 {
 	rank: "queen",
 	suite: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
+	cardImage: "images/queen-of-diamonds.png",
+	cardId: 1,
+	matched: false
 }, 
 {
 	rank: "king",
 	suite: "hearts",
-	cardImage: "images/king-of-hearts.png"
+	cardImage: "images/king-of-hearts.png",
+	cardId: 2,
+	matched: false
 },
 {
 	rank: "king",
 	suite: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
+	cardImage: "images/king-of-diamonds.png",
+	cardId: 3,
+	matched: false
 }
 ];
 var cardsInPlay = [];
 
 var checkForMatch = function () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+	if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
 		alert("you found a match!");
+		cards[cardsInPlay[0].cardId].matched = true;
+		cards[cardsInPlay[1].cardId].matched = true;
 	} else {
 		alert("sorry, try again!")
+		setTimeout(resetBoard, 1000);
 	}
 	cardsInPlay = [];
 };
@@ -35,7 +46,7 @@ var flipCard = function () {
 	var cardId = this.getAttribute('data-id');
 	console.log("user flipped " + cards[cardId].rank + "\nsuit: " + cards[cardId].suite + "\nimage: " + cards[cardId].cardImage);
 	this.setAttribute('src', cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
+	cardsInPlay.push(cards[cardId]);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
@@ -48,6 +59,15 @@ var createBoard = function() {
 		cardElement.setAttribute('data-id', i);
 		cardElement.addEventListener('click', flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+var resetBoard = function() {
+	var cardElements = document.querySelectorAll(".board img");
+	for (var i = 0; i < cardElements.length; i++) {
+		if (cards[i].matched == false) {
+			cardElements[i].setAttribute('src', 'images/back.png');
+		}
 	}
 };
 
